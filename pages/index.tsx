ff45a7c28e2ff660bb0a3ab029325037127197b8
Kyunghwan Ko <kyunghwanko1207@gmail.com>
2023-05-01 14:52:57 +0900

    init: inital commit message

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ export default function Home() {
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
+            <div>hi</div>
           </p>
           <div>
             <a
