254453f003315c6f5728c9b0cf38b48609ae997a
Kyunghwan Ko <kyunghwanko1207@gmail.com>
2023-05-04 16:41:26 +0900

    feat: implement cart adding

---
+++ types/CartType.ts
@@ -0,0 +1,9 @@
+export interface CartType {
+    product_id: number,
+    product_code: string,
+    name: string,
+    price: number,
+    image_url: string,
+    description: string,
+    qty: number
+}
