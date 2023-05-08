43664e19ff9e7cedb4ab7023a9de4ee66c09fd5b
Nabom2 <schneewit@gmail.com>
2023-05-08 11:12:41 +0900

    ui : modify page structure

--- pages/points.tsx
+++ pages/points.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Points.module.css'
 import Link from 'next/link'
 import { NextPageWithLayout } from './_app'
-import MainLayout from '@/components/layouts/mainLayout'
+import PointsLayout from '@/components/layouts/pointsLayout'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,53 +17,25 @@ const Points: NextPageWithLayout = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      {/* <main className={styles.main}>
 
         <div className={styles.description}>
           <h1>
             신세계포인트 적립방법을 선택해 주세요
           </h1>
         </div>
-
-        <div className={styles.center}>
-          
-          <div className={styles.phoneNumber_btn}>
-              <button>전화번호 입력</button>
-          </div>
-          
-          <div className={styles.Barcode_btn}>
-              <button>바코드 스캔</button>
-          </div>
-          
-          <div className={styles.pointCard_btn}>
-              <button>포인트카드 센싱</button>
-          </div>
-          
-          <div className={styles.pass_btn}>
-            <Link href='/ssgService'>
-              <button>적립 안함</button>
-            </Link>
-          </div>
-
-        </div>
-
-        <div className={styles.pre_btn}>
-            <Link href='/carts'>
-                <button>이전단계</button>
-            </Link>
-        </div>
-        
-      </main>
+      </main> */}
     </>
+    
   );
 }
 
 Points.getLayout = function getLayout(page: React.ReactNode) {
   return(
     <>
-    <MainLayout>
+    <PointsLayout>
       {page}
-    </MainLayout>
+    </PointsLayout>
     </>
   )
 }
--- pages/shoppingBag.tsx
+++ pages/shoppingBag.tsx
@@ -3,10 +3,12 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/ShoppingBag.module.css'
 import Link from 'next/link'
+import ShoppingBagLayout from '@/components/layouts/shoppingBagLayout'
+import { NextPageWithLayout } from './_app'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function ShoppingBag() {
+const Shoppingbag: NextPageWithLayout = () => {
   return (
     <>
       <Head>
@@ -17,39 +19,19 @@ export default function ShoppingBag() {
       </Head>
       <main className={styles.main}>
 
-        <div className={styles.description}>
-          <h1>
-            필요하신 쇼핑백 선택해 주세요
-          </h1>
-        </div>
-
-        <div className={styles.center}>
-          
-          <div className={styles.paperBag_btn}>
-            {/* prices have applied at payment step */}
-              <button>종이봉투</button>
-          </div>
-          
-          <div className={styles.garbageBag_btn}>
-            {/* prices have applied at payment step */}
-              <button>종량제봉투</button>
-          </div>
-          
-          <div className={styles.pass_btn}>
-            <Link href='/payments'>
-              <button>필요 없음</button>
-            </Link>
-          </div>
-
-        </div>
-
-        <div className={styles.pre_btn}>
-            <Link href='/ssgservice'>
-                <button>이전단계</button>
-            </Link>
-        </div>
-        
       </main>
     </>
   )
 }
+
+Shoppingbag.getLayout = function getLayout(page: React.ReactNode) {
+  return(
+    <>
+    <ShoppingBagLayout>
+      {page}
+    </ShoppingBagLayout>
+    </>
+  )
+}
+
+export default Shoppingbag
