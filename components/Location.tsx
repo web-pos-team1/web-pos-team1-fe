6974b4e56d62f2fd85faefb7ac00998c8bd6f138
Nabom2 <schneewit@gmail.com>
2023-05-08 11:07:26 +0900

    ui : create components

--- components/Location.tsx
+++ components/Location.tsx
@@ -1,18 +1,73 @@
-import React from 'react'
+// import React from 'react'
+// import style from './Location.module.css';
+
+// export default function Location() {
+//   function handleItemClick(e: React.MouseEvent<HTMLLIElement>) {
+//     const target = e.target as HTMLLIElement;
+//     console.log(`Selected item: ${target.innerText}`)
+//   }
+//   return (
+//     <div className={style.wrapper}>
+//         <nav>
+//             <ul>
+//                 <li onClick={handleItemClick}>1 상품등록</li>
+//                 <li onClick={handleItemClick}>2 포인트적립</li>
+//                 <li onClick={handleItemClick}>3 배송/선물</li>
+//                 <li onClick={handleItemClick}>4 결제</li>
+//                 <li onClick={handleItemClick}>5 완료</li>
+//             </ul>
+//         </nav>
+//     </div>
+//   )
+// }
+
+import React, { useState } from 'react';
 import style from './Location.module.css';
 
 export default function Location() {
+  const [activeIndex, setActiveIndex] = useState(0); // 초기 선택한 index를 0으로 설정
+
+  const handleItemClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className={style.wrapper}>
-        <nav>
-            <ul>
-                <li>1 상품등록</li>
-                <li>2 포인트적립</li>
-                <li>3 배송/선물</li>
-                <li>4 결제</li>
-                <li>5 완료</li>
-            </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li
+            className={activeIndex === 0 ? style.active : ''}
+            onClick={() => handleItemClick(0)}
+          >
+            1 상품등록
+          </li>
+          <li
+            className={activeIndex === 1 ? style.active : ''}
+            onClick={() => handleItemClick(1)}
+          >
+            2 포인트적립
+          </li>
+          <li
+            className={activeIndex === 2 ? style.active : ''}
+            onClick={() => handleItemClick(2)}
+          >
+            3 배송/선물
+          </li>
+          <li
+            className={activeIndex === 3 ? style.active : ''}
+            onClick={() => handleItemClick(3)}
+          >
+            4 결제
+          </li>
+          <li
+            className={activeIndex === 4 ? style.active : ''}
+            onClick={() => handleItemClick(4)}
+          >
+            5 완료
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
+
---
+++ components/Text.tsx
@@ -0,0 +1,15 @@
+import style from './Text.module.css'
+
+interface Props {
+    text: string
+}
+
+const Text: React.FC<Props> = ({ text }) => {
+    return (
+        <div className={style.text}>
+            {text}
+        </div>
+    )
+}
+
+export default Text;
