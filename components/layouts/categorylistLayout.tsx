600f5804cfbd0d89642b7784f78e3d03e8a080d8
Nabom2 <schneewit@gmail.com>
2023-05-10 14:59:07 +0900

    ui : category list page (create button & category list layout)

--- components/layouts/categorylistLayout.tsx
+++ components/layouts/categorylistLayout.tsx
@@ -11,15 +11,33 @@ export default function CategorylistLayout(props:{children:React.ReactNode}) {
             {props.children}
             <Text text="카테고리를 선택해 주세요" />
             <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridGap: '40px', margin: '85px 40px 160px 40px' }}>
-                <Button src="/images/fruit.png" alt="fruit category" text="과일" onClick={() => console.log('btn 1')}/>
-                <Button src="/images/vegi.png" alt="vigetable category" text="채소" onClick={() => console.log('btn 2')}/>
-                <Button src="/images/chicken.png" alt="meat&eggs category" text="정육 / 계란" onClick={() => console.log('btn 3')}/>
-                <Button src="/images/seafood.png" alt="seafood category" text="수산" onClick={() => console.log('btn 4')}/>
-                <Button src="/images/rice.png" alt="rice&nuts category" text="쌀 / 견과" onClick={() => console.log('btn 5')}/>
-                <Button src="/images/milk.png" alt="milk&daily products category, " text="우유 / 유제품" onClick={() => console.log('btn 6')}/>
-                <Button src="/images/snack.png" alt="snack category" text="간식" onClick={() => console.log('btn 7')}/>
-                <Button src="/images/sauce.png" alt="sauce&oil category" text="소스 / 오일" onClick={() => console.log('btn 8')}/>
-                <Button src="/images/gift.png" alt="gift category" text="선물류" onClick={() => console.log('btn 9')}/>
+                <Link href="/products/과일">
+                    <Button src="/images/fruit.png" alt="fruit category" text="과일" onClick={() => console.log('btn 1')}/>
+                </Link>
+                <Link href="/products/채소">
+                    <Button src="/images/vegi.png" alt="vigetable category" text="채소" onClick={() => console.log('btn 2')}/>
+                </Link>
+                <Link href="/products/정육-계란">
+                    <Button src="/images/chicken.png" alt="meat&eggs category" text="정육 / 계란" onClick={() => console.log('btn 3')}/>
+                </Link>
+                <Link href="/products/수산">
+                    <Button src="/images/seafood.png" alt="seafood category" text="수산" onClick={() => console.log('btn 4')}/>
+                </Link>
+                <Link href="/products/쌀-견과">
+                    <Button src="/images/rice.png" alt="rice&nuts category" text="쌀 / 견과" onClick={() => console.log('btn 5')}/>
+                </Link>
+                <Link href="/products/우유-유제품">
+                    <Button src="/images/milk.png" alt="milk&daily products category, " text="우유 / 유제품" onClick={() => console.log('btn 6')}/>
+                </Link>
+                <Link href="/products/간식">
+                    <Button src="/images/snack.png" alt="snack category" text="간식" onClick={() => console.log('btn 7')}/>
+                </Link>
+                <Link href="/products/소스-오일">
+                    <Button src="/images/sauce.png" alt="sauce&oil category" text="소스 / 오일" onClick={() => console.log('btn 8')}/>
+                </Link>
+                <Link href="/products/선물류">
+                    <Button src="/images/gift.png" alt="gift category" text="선물류" onClick={() => console.log('btn 9')}/>
+                </Link>
             </div>
         </div>
     )
