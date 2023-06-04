5fd30ff3cc3fa4f8df49209f1f5e665826bf6c23
Kyunghwan Ko <kyunghwanko1207@gmail.com>
2023-06-04 17:08:15 +0900

    feat: impl makeUID() function to generate unique UID each payments

--- components/AlertModal/PhoneMismatch.tsx
+++ components/AlertModal/PhoneMismatch.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
-import style from "./AlertModal.module.css";
+import style from "./PhoneMatchAlertModal.module.css";
 import Image from 'next/image';
 
 export default function PhoneMismatch(props:{show:boolean, onClose:Dispatch<SetStateAction<boolean>>}) {
