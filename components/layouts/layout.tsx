caaf0d4b9c94344dba105aebadb3fd103a8fea74
Nabom2 <schneewit@gmail.com>
2023-06-11 19:23:34 +0900

    chore : gift card components

--- components/layouts/layout.tsx
+++ components/layouts/layout.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Header from "@/components/Header";
-import PhoneNumber from "../PhoneNumber";
-import { RecoilRoot } from "recoil";
 
 export default function Layout(props:{children:React.ReactNode}) {
     return (
