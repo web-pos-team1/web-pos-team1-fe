a9210d16dc3052b42e93262c576c8b67bec3e20e
Kyunghwan Ko <kyunghwanko1207@gmail.com>
2023-06-12 11:41:58 +0900

    feat: make disabled ssg-service when totalPrice under 30000won

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,11 +7,12 @@ interface Props {
     alt: string;
     text?: string;
     style? : React.CSSProperties;
+    styleText?: string;
 }
 
-const Button: React.FC<Props> = ({ onClick, src, alt, text }) => {
+const Button: React.FC<Props> = ({ onClick, src, alt, text, styleText }) => {
     return (
-        <button className={style.btn} onClick={onClick}>
+        <button className={`${styleText && styleText == 'style.lower' ? style.lower : style.btn }`} onClick={onClick}>
             <div className={style.content}>
                 <Image src={src} alt={alt} width={141} height={140}/>
                 <span className={style.text}>
